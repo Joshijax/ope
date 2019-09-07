4ece6bd2ae7667a082df84f7d299ff2cd3924f5d
joshijax <joshijax08@gmail.com>
2019-09-07 19:51:38 +0100

    first commit

---
+++ mail.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+My name is <?php echo $_POST["name"]; ?><br>
+Your email address is  <?php echo $_POST["email"]; ?><br>
+My Title is <?php echo $_POST["email"]; ?><br><br><hr>
+<?php echo $_POST["email"]; ?>
+
+
+</body>
+</html> 
